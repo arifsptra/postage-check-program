e0de8f5b8f365ab7e6cc2af3d36e7209b6ae3da4
arifsptra <arif.sptrra@gmail.com>
2022-07-07 11:21:11 +0700

    debugging in function file

--- function.php
+++ function.php
@@ -1,76 +1,81 @@
 <?php
 
 class Shipping {
-    private $key = '64ab98b0920dba22d170a4580ae1320b';
-    private $city_url = 'https://api.rajaongkir.com/starter/city';
-    private $cost_url = 'https://api.rajaongkir.com/starter/cost';
+  private $key      = '64ab98b0920dba22d170a4580ae1320b'; // API Key from rajaongkir
+  private $city_url = 'https://api.rajaongkir.com/starter/city'; // url to take city date
+  private $cost_url = 'https://api.rajaongkir.com/starter/cost'; // url to take cost shipping
 
-    function array_sort_by_column($arr, $col, $dir = SORT_DESC){
-        $sort_col = [];
-        foreach($arr as $key => $value){
-            $sort_col[$key] = $value[$col];
-        }
-        array_multisort($sort_col, $dir, $arr);
+  // function for sort array
+  function array_sort_by_column(&$arr, $col, $dir = SORT_DESC) {
+    $sort_col = array();
+    foreach ($arr as $key => $row) {
+      $sort_col[$key] = $row[$col];
     }
 
-    function get_city(){
-        $curl = curl_init();
+    array_multisort($sort_col, $dir, $arr);
+  }
 
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => $this->city_url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_HTTPHEADER => array(
-            "key: {$this->key}"
-        ),
-        ));
+  // function to take city date 
+  function get_city()
+  {
+    $curl = curl_init();
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $this->city_url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => array(
+        "key: {$this->key}"
+      ),
+    ));
 
-        curl_close($curl);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-        if ($err) {
-            return "cURL Error #:" . $err;
-        } else {
-            return $response;
-        }
+    curl_close($curl);
+
+    if ($err) {
+      return "cURL Error #:" . $err;
+    } else {
+      return $response;
     }
+  }
 
+  // to retrieve shipping cost data based on city of origin, city of destination, weight and courier
+  function get_cost($id_kota_asal, $id_kota_tujuan, $berat, $courir)
+  {
+    $curl = curl_init();
 
-    function get_cost($id_kota_asal, $id_kota_tujuan, $berat, $kurir){
-        $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $this->cost_url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => "origin={$id_kota_asal}&destination={$id_kota_tujuan}&weight={$berat}&courier=$courir",
+      CURLOPT_HTTPHEADER => array(
+        "content-type: application/x-www-form-urlencoded",
+        "key: {$this->key}"
+      ),
+    ));
 
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => $this->cost_url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => "origin={$id_kota_asal}&destination=($id_kota_tujuan)&weight={$berat}&courier={$kurir}",
-        CURLOPT_HTTPHEADER => array(
-            "content-type: application/x-www-form-urlencoded",
-            "key: {$this->key}"
-        ),
-        ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+    curl_close($curl);
 
-        curl_close($curl);
-
-        if ($err) {
-            return "cURL Error #:" . $err;
-        } else {
-            return $response;
-        }
+    if ($err) {
+      return "cURL Error #:" . $err;
+    } else {
+      return $response;
     }
+  }
 }
 
 ?>
